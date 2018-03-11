30347b54a411a8aff311de2591d8ca8f73bcafbf
Anna Wong <annajwong@gmail.com>
2018-03-11 00:57:04 -0800

    added stuff for viewing individual events

---
+++ MenloHacksIII/MenloHacksIII/View Controllers/EventViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EventViewController.swift
+//  MenloHacksIII
+//
+//  Created by Anna Wong on 3/11/18.
+//  Copyright © 2018 annawong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EventViewController: UIViewController
+{
+    
+}
